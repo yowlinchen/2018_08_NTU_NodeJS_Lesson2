b375bc82a7f02d716a473155a1e2f97720380437
yowlinchen <terry.y.chen@intel.com>
2018-08-11 17:15:55 +0800

    send a html page with picture

--- project/index.js
+++ project/index.js
@@ -14,14 +14,15 @@ app.get("/image", function (req, res) {
 
 // Take care of / request
 app.get("/", function (req, res) {
+    res.send("<h3>My Image</h3><img src='/image' />");
     // set rsponse header
-    res.set("test", "my header");
-    let obj = {
-        x: 3,
-        y: 4
-    };
+    // res.set("test", "my header");
+    // let obj = {
+    //     x: 3,
+    //     y: 4
+    // };
+    // // res.send(obj);
     // res.send(obj);
-    res.send(obj);
 });
 
 // start the server at http://localhost:3000/
