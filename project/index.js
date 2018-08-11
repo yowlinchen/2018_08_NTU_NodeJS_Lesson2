f72d0c543014911364bb0f4fe94a7966776c15ea
yowlinchen <terry.y.chen@intel.com>
2018-08-11 16:59:06 +0800

    using json

--- project/index.js
+++ project/index.js
@@ -7,15 +7,23 @@ let express = require("express");
 // create applicatoin object
 let app = express();
 
-// take care of /test request
-app.get("/test", function (req, res) {
-    res.send("<b>Hello</b> Test <script>alert('test');</script>");
+app.get("/", function (req, res) {
+    let obj = {
+        x: 3,
+        y: 4
+    };
+    res.send(obj);
 });
 
-// take care path / requirement
-app.get("/", function (req, res) {
-    res.send("Hello Node.js");
-});
+// take care of /test request
+// app.get("/test", function (req, res) {
+//     res.send("<b>Hello</b> Test <script>alert('test');</script>");
+// });
+
+// // take care path / requirement
+// app.get("/", function (req, res) {
+//     res.send("Hello Node.js");
+// });
 
 // start the server at http://localhost:3000/
 app.listen(3000, function () {
