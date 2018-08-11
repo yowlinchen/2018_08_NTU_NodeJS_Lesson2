80a54b665c636be6fcce7bfbee26ee5a81b4aaf7
yowlinchen <terry.y.chen@intel.com>
2018-08-11 09:57:48 +0800

    continue porject from last time

---
+++ project/index.js
@@ -0,0 +1,23 @@
+// can't use HTML DOM, for example alert()
+// let x = 3;
+// console.log(x);
+
+// load express package 
+let express = require("express");
+// create applicatoin object
+let app = express();
+
+// take care of /test request
+app.get("/test", function (req, res) {
+    res.send("<b>Hello</b> Test <script>alert('test');</script>");
+});
+
+// take care path / requirement
+app.get("/", function (req, res) {
+    res.send("Hello Node.js");
+});
+
+// start the server at http://localhost:3000/
+app.listen(3000, function () {
+    console.log("Server Started");
+})
